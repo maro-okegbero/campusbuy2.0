02f3f4ec88f1a3ed1dba1fafe2bee385376e2b11
Maro <marookegbero@gmail.com>
2020-05-09 21:49:08 +0100

    cloudinary configs

--- campusbuy2_0/wsgi.py
+++ campusbuy2_0/wsgi.py
@@ -11,9 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campusbuy2_0.settings')
 
 application = get_wsgi_application()
-
+#
+# application = DjangoWhiteNoise(application)
 
 
