fdc9e6254cdfa6c54544c79f1315f5b3693a1367
Maro <marookegbero@gmail.com>
2020-06-13 18:42:37 +0100

    minor fixes

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+admin.site.site_header = 'CAMPUSBUY god MODE'
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(School)
